095639ca4f2b741bcce5a72e73684982d0de6ebf
nbuy <nobuhiro.yasutomi@nifty.ne.jp>
2008-02-29 06:09:29 +0000

    fix eval status logging message. add notify when evaluation.

--- status.php
+++ status.php
@@ -1,6 +1,6 @@
 <?php
 // Changing message status
-// $Id: status.php,v 1.7 2007/11/01 05:01:15 nobu Exp $
+// $Id: status.php,v 1.8 2008/02/29 06:09:29 nobu Exp $
 
 include "../../mainfile.php";
 include "functions.php";
@@ -15,10 +15,15 @@ if (!empty($_POST['eval'])) {	// evaluate at last
     $com = $xoopsDB->quoteString($myts->stripSlashesGPC($_POST['comment']));
     $now = time();
     if (is_cc_evaluate($msgid, $uid, $pass)) {
+	$res = $xoopsDB->query("SELECT fidref,status FROM ".CCMES." WHERE msgid=$msgid");
+	list($formid, $s) = $xoopsDB->fetchRow($res);
 	$xoopsDB->query("UPDATE ".CCMES." SET comment=$com,value=$eval,comtime=$now,status='c' WHERE msgid=$msgid");
-	$res = $xoopsDB->query("SELECT fidref FROM ".CCMES." WHERE msgid=$msgid");
-	list($formid) = $xoopsDB->fetchRow($res);
-	cc_log_message($formid, _MD_EVALS." ($eval)", $msgid);
+	$log = _MD_EVALS." ($eval)";
+	$log .= "\n".sprintf(_CC_LOG_STATUS, $msg_status[$s], $msg_status[_STATUS_CLOSE]);
+	$tags = array('X_COMMENT_URL'=>_MD_EVALS." ($eval)\n".XOOPS_URL."/modules/".basename(dirname(__FILE__))."/message.php?id=$msgid");
+	$notification_handler =& xoops_gethandler('notification');
+	$notification_handler->triggerEvent('message', $msgid, 'comment', $tags);
+	cc_log_message($formid, $log, $msgid);
 	redirect_header($redirect, 1, _MD_EVAL_THANKYOU);
     } else {
 	redirect_header($redirect, 3, _NOPERM);

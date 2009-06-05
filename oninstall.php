f06e61906b1a5d346d4ed177c76de959aff34638
nbuy <nobuhiro.yasutomi@nifty.ne.jp>
2009-06-05 09:12:31 +0000

    compat for XOOPS 2.3

--- oninstall.php
+++ oninstall.php
@@ -1,18 +1,23 @@
 <?php
 # ccenter module onInstall proceeding.
-# $Id: oninstall.php,v 1.1 2008/05/17 05:55:47 nobu Exp $
+# $Id: oninstall.php,v 1.2 2009/06/05 09:12:31 nobu Exp $
 
-global $xoopsDB, $xoopsUser;
+register_shutdown_function('ccenter_sample_form');
 
-define('FORM', $xoopsDB->prefix('ccenter_form'));
+function ccenter_sample_form()
+{
+    global $xoopsDB, $xoopsUser, $msgs;
 
-$data = array('mtime'=>time(),
-	      'title'=>$xoopsDB->quoteString(_MI_SAMPLE_TITLE),
-	      'description'=>$xoopsDB->quoteString(_MI_SAMPLE_DESC),
-	      'grpperm'=>"'|".XOOPS_GROUP_ANONYMOUS."|".XOOPS_GROUP_USERS."|'",
-	      'defs'=>$xoopsDB->quoteString(_MI_SAMPLE_DEFS),
-	      'priuid'=>$xoopsUser->getVar('uid'));
+    define('FORM', $xoopsDB->prefix('ccenter_form'));
 
-$xoopsDB->query('INSERT INTO '.FORM."(".join(',', array_keys($data)).")VALUES(".join(',', $data).")");
-$msgs[] = '&nbsp;&nbsp;<b>'._MI_SAMPLE_FORM."</b>";
+    $data = array('mtime'=>time(),
+		  'title'=>$xoopsDB->quoteString(_MI_SAMPLE_TITLE),
+		  'description'=>$xoopsDB->quoteString(_MI_SAMPLE_DESC),
+		  'grpperm'=>"'|".XOOPS_GROUP_ANONYMOUS."|".XOOPS_GROUP_USERS."|'",
+		  'defs'=>$xoopsDB->quoteString(_MI_SAMPLE_DEFS),
+		  'priuid'=>$xoopsUser->getVar('uid'));
+
+    $xoopsDB->query('INSERT INTO '.FORM."(".join(',', array_keys($data)).")VALUES(".join(',', $data).")");
+    $msgs[] = '&nbsp;&nbsp;<b>'._MI_SAMPLE_FORM."</b>";
+}
 ?>

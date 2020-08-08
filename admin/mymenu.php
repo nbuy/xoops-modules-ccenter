1101b0c5c33e64239f979dfc0ad60f4f0f5a9bcb
Nobuhiro YASUTOMI <nobuhiro.yasutomi@nifty.ne.jp>
2020-08-08 18:16:30 +0900

    instance to be reference in PHP 7.4 later

--- admin/mymenu.php
+++ admin/mymenu.php
@@ -76,7 +76,7 @@ foreach( array_keys( $adminmenu ) as $i ) {
 if ( $use_altsys ) {
 	// display
 	require_once XOOPS_ROOT_PATH.'/class/template.php' ;
-	$tpl =& new XoopsTpl() ;
+	$tpl = new XoopsTpl() ;
 	$tpl->assign( array(
 						'adminmenu' => $adminmenu ,
 						) ) ;
